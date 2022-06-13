2b3479b49bfff2bce324a7ffd74ef8a1c674ae6d
Nanalash11 <84817687+Nanalash11@users.noreply.github.com>
2022-06-13 01:17:23 -0400

    Landing Page

---
+++ Remake-ALVSH copy/index.js
@@ -0,0 +1,7 @@
+var modal = document.getElementById("bottom-section");
+
+var btn = document.getElementById("serviceBtn");
+
+var buttonClicked = btn.addEventListener("click", function(){
+    modal.style.display = block;
+})
