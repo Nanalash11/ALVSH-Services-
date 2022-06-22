2303df52b25f1602419898adfaa4a3c1652babdc
Nanalash11 <84817687+Nanalash11@users.noreply.github.com>
2022-06-21 20:41:38 -0400

    edits to services section
    Added new icons to services sections to bring out more color

--- Remake-ALVSH copy/index.js
+++ Remake-ALVSH copy/index.js
@@ -1,7 +1,20 @@
-var modal = document.getElementById("bottom-section");
+
+/*****
+var modal = document.getElementsByClassName("bottom-section");
 
 var btn = document.getElementById("serviceBtn");
 
 var buttonClicked = btn.addEventListener("click", function(){
     modal.style.display = block;
+})
+*******/
+
+var btnInfoDisplay = document.getElementById('info-display');
+console.log(btnInfoDisplay);
+
+var bottomBtn = document.querySelector('#charity-btn');
+console.log(bottomBtn);
+
+var buttonClicked = bottomBtn.addEventListener("click", function() {
+    btnInfoDisplay.style.display = "block";
 })
