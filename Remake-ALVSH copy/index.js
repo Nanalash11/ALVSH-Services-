f23ef3127e9f2b4e353d51c986877e841df5bf33
NanaBoakye11 <nb74@njit.edu>
2022-07-11 14:59:10 -0400

    Modified Landing Page and Services

--- Remake-ALVSH copy/index.js
+++ Remake-ALVSH copy/index.js
@@ -9,12 +9,55 @@ var buttonClicked = btn.addEventListener("click", function(){
 })
 *******/
 
+/*************** When "Learn More" is clicked at bottom section **************/
+
 var btnInfoDisplay = document.getElementById('info-display');
-console.log(btnInfoDisplay);
 
 var bottomBtn = document.querySelector('#charity-btn');
-console.log(bottomBtn);
 
 var buttonClicked = bottomBtn.addEventListener("click", function() {
     btnInfoDisplay.style.display = "block";
 })
+
+
+/*************** When "Services" is clicked at top section **************/
+
+/* var serviceSection = document.getElementsByClassName('middle-section');
+var serviceBtn = document.getElementById('serviceBtn');
+
+var serviceBtnClicked = serviceBtn.addEventListener("click", function(){
+   serviceSection. .display =;    //
+})
+
+
+/*************** When "Services" is clicked at middle section **************/
+
+// get amount of images in service section **/
+
+var servicesOptions = document.querySelectorAll("#service-img").length;
+var firstImage = document.querySelectorAll(".support-para");
+var secondImage = document.querySelectorAll(".web-para");
+var thirdImage = document.querySelectorAll(".edu-para");
+
+for (var i = 0; i < servicesOptions; i++) {
+    document.querySelector('.service-img')[i].addEventListener("click", function(event){
+        console.log(event);
+
+ /***** if (this.event === firstImage) {
+       document.querySelector(".support-para").style.display = "block";
+       if (this.event === secondImage){
+           document.querySelector(".web-para").style.display = "block";
+       }
+       
+   } else {
+       console.log("Not found")
+   } */  
+
+
+
+
+
+
+    })
+
+}
